56035be794198b58f6cb865fe729ab7d2295c5e3
sweetbeantastebun <58278315+sweetbeantastebun@users.noreply.github.com>
2021-02-16 15:04:26 +0900

    Update STFT3.py

--- STFT3.py
+++ STFT3.py
@@ -18,16 +18,16 @@ import pandas as  pd  #数式、配列を操作するライブラリ
 import psutil  #メモリ、CPUの使用率をモニターするライブラリ
 
 #グラフのリアルタイムプロットの更新時間数
-Loop_count_Value1 = 5
-Loop_count_Value2 = 10
+Loop_count_Value1 = 250
+Loop_count_Value2 = 1000
 #しきい値を指定
-threshold_value_MAX = 0.13
-threshold_value_MIN = 0.005
+threshold_value_MAX = 0.013
+threshold_value_MIN = 0.009
 #FFT検出強度のフィルタリング
 noise_reduction_filters = 0
 #カラーバーのレンジ指定
-vmin = -300
-vmax = 200
+vmin = -10
+vmax = 10
 
 t00 = time.time()
 path = "/home/pi/Documents/admp441_data/"  #ディレクトリ先を変数pathに格納(データの格納先デレクトリを読み出すときに使用する)
@@ -95,18 +95,18 @@ def Recording_A():
         spectrum_A = spectrum_A[:int(spectrum_A.shape[0]/2)]    #スペクトルがマイナスになるスペクトル要素の削除
         frequency_A = frequency_A[:int(frequency_A.shape[0]/2)]    #周波数がマイナスになる周波数要素の削除    
         t5 = time.time()
-        print("Recording_A", t5-t0)
+        #print("Recording_A", t5-t0)
         """STFT"""
         #フレーム数の指定
         NN = 8192
         #周波数、時間軸、スペクトル強度を算出する
         freqs, times, Sx = signal.spectrogram(samples_N, fs = fs_A, window = "hanning",
                                               nperseg = NN,  #nperseg:分割数
-                                              noverlap = NN - NN/16,  #フレームの重なり具合。
+                                              noverlap = NN/8,  #フレームの重なり具合。
                                               #noverlap = NN/16,  #フレームの重なり具合
                                               detrend = False, scaling = "spectrum") # スペクトログラム変数
         t6 = time.time()
-        print("signal_spectrogram", t6-t5)
+        #print("signal_spectrogram", t6-t5)
         
         
 def Graph_A1():
@@ -130,8 +130,8 @@ def Graph_A1():
     plt.grid(which="both")
     No1.set_data(sample_of_numbers, RMS_data)
     plt.xlim(sample_of_numbers[-Loop_count_Value1], sample_of_numbers[-1])
-    plt.ylim(0, 0.15)
-    #plt.ylim(0, 0.05)
+    #plt.ylim(0, 0.15)
+    plt.ylim(0, 0.05)
     #print("RMS_data", RMS_data)
     #print("RMS_data", len(RMS_data))
     #print("index_loop", index_loop)
@@ -145,21 +145,20 @@ def Graph_A1():
     plt.xlabel("freqency(Hz)", fontsize=8)
     plt.ylabel("Amplitude Spectrum", fontsize=8)
     """
-    plt.pcolormesh(times, freqs, 10* np.log(Sx), cmap='jet', vmin=vmin, vmax=vmax)
+    plt.pcolormesh(times, freqs, np.log10(Sx), cmap='jet', vmin=vmin, vmax=vmax)
     #plt.pcolormesh(times, freqs, 10* np.log(Sx), cmap="jet")
-    plt.ylim([0, 1000])
-    plt.ylabel("Frequency[Hz]", fontsize=10)
-    plt.xlabel("Time[s]", fontsize=10)
-    plt.xticks(fontsize = 9)
-    plt.yticks(fontsize = 9)
+    plt.ylim([0, fs_A/2])
+    plt.ylabel("Frequency[Hz]", fontsize= 8)
+    plt.xlabel("Time[s]", fontsize= 8)
+    plt.xticks(fontsize = 8)
+    plt.yticks(fontsize = 8)
     plt.colorbar(aspect=40, pad=0.02)
-    
     plt.subplots_adjust(wspace=0.3, hspace=0.3)  #隣接グラフとの隙間
     plt.draw()
     plt.pause(0.01) 
     t18 = time.time()
     if rms_A >= threshold_value_MAX:
-        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"Maximum_value"".png")
+        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"MAX_value"".png")
         file =  filename_A + ".wav"
         shutil.copy(path + file , path2)  #wavファイルをコピーして指定ディレクトリへ移動
         #1サンプル中のMax値
@@ -167,20 +166,20 @@ def Graph_A1():
         #1サンプル中の波高率
         Wave_height_rate_A = rms_A / Maximum_audio_signal_A
         #csvファイルに書き込むフレーム作成
-        header_names = [["Maximum_value", "Wave_height_rate", "RMS"],
+        header_names = [["MAX_value", "Wave_height_rate", "RMS"],
         [round(Maximum_audio_signal_A,4), round(Wave_height_rate_A,4), round(rms_A,4)]]
         #csv作成
-        with open("/home/pi/Documents/admp441_data/"+filename_A+"Maximum_value"+".csv", "w", newline="", encoding="utf-8") as f:
+        with open("/home/pi/Documents/admp441_data/"+filename_A+"MAX_value"+".csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerows(header_names)
     
     if rms_A <= threshold_value_MIN:
-        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"Minimum_value"".png")
+        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"MIN_value"".png")
         file =  filename_A + ".wav"
         shutil.copy(path + file , path2)  #wavファイルをコピーして指定ディレクトリへ移動
     
     t19 = time.time()
-    print("Greph_A", t19-t10)
+    #print("Greph_A", t19-t10)
 
 
 def job_A():
@@ -189,8 +188,8 @@ def job_A():
     plt.subplot(2, 1, 1)
     No1, = plt.plot(sample_of_numbers, RMS_data, lw=1)
     plt.xlim(sample_of_numbers[-Loop_count_Value2], sample_of_numbers[-1])
-    plt.ylim(0, 0.15)
-    #plt.ylim(0, 0.05)
+    #plt.ylim(0, 0.15)
+    plt.ylim(0, 0.05)
     plt.xticks(fontsize = 8)
     plt.yticks(fontsize = 8)
     plt.xlabel("Sample Number", fontsize=8)
@@ -198,7 +197,6 @@ def job_A():
     plt.grid(which="both")
     No1.set_data(sample_of_numbers, RMS_data)
     plt.subplot(2, 1, 2)
-    """
     plt.plot(frequency_A, np.abs(spectrum_A), lw=1)
     plt.axis([0,1600, 0,50])
     plt.xticks(fontsize = 8)
@@ -207,7 +205,7 @@ def job_A():
     plt.xlabel("freqency(Hz)", fontsize=8)
     plt.ylabel("Amplitude Spectrum", fontsize=8)
     """
-    plt.pcolormesh(times, freqs, 10* np.log(Sx), cmap='jet', vmin=vmin, vmax=vmax)
+    plt.pcolormesh(times, freqs, np.log10(Sx), cmap='jet', vmin=vmin, vmax=vmax)
     #plt.pcolormesh(times, freqs, 10* np.log(Sx), cmap="jet")
     plt.ylim([0, 1000])
     plt.ylabel("Frequency[Hz]", fontsize=10)
@@ -215,7 +213,7 @@ def job_A():
     plt.xticks(fontsize = 9)
     plt.yticks(fontsize = 9)
     plt.colorbar(aspect=40, pad=0.02)
-    
+    """
     plt.subplots_adjust(wspace=0.3, hspace=0.3)  #隣接グラフとの隙間
     plt.savefig("/home/pi/Documents/admp441_data/"+filename_A+"cron"".png")
     plt.close()
@@ -233,7 +231,7 @@ def job_A():
     cpu = psutil.cpu_percent(interval = 1)  #cpu使用率を出力
     disk = psutil.disk_usage("/")  #disk容量
     #csvファイルに書き込むフレーム作成
-    header_names = [["Maximum_value", "Minimum_value", "Average_value", "number_of_peeks", "memory.percent", "cpu", "disk.percent"],
+    header_names = [[Maximum_value", "Minimum_value", "Average_value", "number_of_peeks", "memory.percent", "cpu", "disk.percent"],
     [round(Maximum_value,4), round(Minimum_value,4), round(Average_value,4), peeks, memory.percent, cpu, disk.percent]]
     #csv作成
     with open("/home/pi/Documents/admp441_data/"+filename_A+"cron"+".csv", "w", newline="", encoding="utf-8") as f:
