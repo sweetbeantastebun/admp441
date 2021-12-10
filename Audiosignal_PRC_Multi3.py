a9adcbe428485dfa52a1a7d4667b6c176c7b0eb5
sweetbeantastebun <58278315+sweetbeantastebun@users.noreply.github.com>
2021-12-10 13:58:33 +0900

    Update Audiosignal_PRC_Multi3.py

--- Audiosignal_PRC_Multi3.py
+++ Audiosignal_PRC_Multi3.py
@@ -38,7 +38,7 @@ def Recording_A():
     #ファイルの名前をタイムスタンプ化する
     global filename_A
     timestamp = datetime.today()
-    filename_A = str(timestamp.year) + str(timestamp.month) + str(timestamp.day) + "_" + str(timestamp.hour) + ":" + str(timestamp.minute) + ":" + str(timestamp.second)
+    filename_A = timestamp.strftime("%Y%m%d%H%M%S")
     #録音実行（16ビット量子化、44.1kHz）
     record = "arecord -d 1 -f S16_LE -r 44100 /home/pi/Documents/admp441_data_A/"+filename_A+".wav"
     subprocess.call(record, shell=True)
@@ -132,7 +132,7 @@ def Recording_B():
     #ファイルの名前をタイムスタンプ化する
     global filename_B
     timestamp = datetime.today()
-    filename_B = str(timestamp.year) + str(timestamp.month) + str(timestamp.day) + "_" + str(timestamp.hour) + ":" + str(timestamp.minute) + ":" + str(timestamp.second)
+    filename_B = timestamp.strftime("%Y%m%d%H%M%S")
     #録音実行（16ビット量子化、44.1kHz）
     record = "arecord -d 1 -f S16_LE -r 44100 /home/pi/Documents/admp441_data_B/"+filename_B+".wav"
     subprocess.call(record, shell=True)
