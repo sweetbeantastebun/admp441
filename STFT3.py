6917baf308c52d24606ec331bf56905ada4d1631
sweetbeantastebun <58278315+sweetbeantastebun@users.noreply.github.com>
2021-12-24 13:33:53 +0900

    Update STFT3.py

--- STFT3.py
+++ STFT3.py
@@ -158,7 +158,7 @@ def Graph_A1():
     plt.pause(0.01) 
     t18 = time.time()
     if rms_A >= threshold_value_MAX:
-        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"MAX_value"".png")
+        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"_MAX"".png")
         file =  filename_A + ".wav"
         shutil.copy(path + file , path2)  #wavファイルをコピーして指定ディレクトリへ移動
         #1サンプル中のMax値
@@ -169,12 +169,12 @@ def Graph_A1():
         header_names = [["MAX_value", "Wave_height_rate", "RMS"],
         [round(Maximum_audio_signal_A,4), round(Wave_height_rate_A,4), round(rms_A,4)]]
         #csv作成
-        with open("/home/pi/Documents/admp441_data/"+filename_A+"MAX_value"+".csv", "w", newline="", encoding="utf-8") as f:
+        with open("/home/pi/Documents/admp441_data/"+filename_A+"_MAX"+".csv", "w", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerows(header_names)
     
     if rms_A <= threshold_value_MIN:
-        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"MIN_value"".png")
+        fig1.savefig("/home/pi/Documents/admp441_data/"+filename_A+"_MIN"".png")
         file =  filename_A + ".wav"
         shutil.copy(path + file , path2)  #wavファイルをコピーして指定ディレクトリへ移動
     
@@ -215,7 +215,7 @@ def job_A():
     plt.colorbar(aspect=40, pad=0.02)
     """
     plt.subplots_adjust(wspace=0.3, hspace=0.3)  #隣接グラフとの隙間
-    plt.savefig("/home/pi/Documents/admp441_data/"+filename_A+"cron"".png")
+    plt.savefig("/home/pi/Documents/admp441_data/"+filename_A+".png")
     plt.close()
     #期間中のMax値
     Maximum_value = np.max(RMS_data)
@@ -234,7 +234,7 @@ def job_A():
     header_names = [[Maximum_value", "Minimum_value", "Average_value", "number_of_peeks", "memory.percent", "cpu", "disk.percent"],
     [round(Maximum_value,4), round(Minimum_value,4), round(Average_value,4), peeks, memory.percent, cpu, disk.percent]]
     #csv作成
-    with open("/home/pi/Documents/admp441_data/"+filename_A+"cron"+".csv", "w", newline="", encoding="utf-8") as f:
+    with open("/home/pi/Documents/admp441_data/"+filename_A+".csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerows(header_names)
     """
